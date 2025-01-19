697ecb0ae2b1b20c988639ebd7f1a3c73aef2e3e
Vinicius Ventura <42718816+ventura-v@users.noreply.github.com>
2025-01-19 01:14:19 +0000

    adiciona pagina inicial

---
+++ pages/blog/first-post.js
@@ -0,0 +1,7 @@
+export default function FirstPost(){
+    return (
+        <div>
+            <h1>Hello First Post</h1>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div>
+            <h1>2025 will be my year!</h1>
+        </div>
+    )
+}
